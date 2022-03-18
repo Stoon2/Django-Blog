b9a6f4d3bc6801e5beaa2ea0026c2a4b57599e05
Menna97 <menna.elgedawy9761@gmail.com>
2022-03-19 00:23:59 +0200

    add post view and template

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from .views import HomeView, PostDetailView
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', HomeView.as_view(), name='home'),
+    path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
     path('login', views.login, name='login'),
     path('signup', views.signup, name='signup'),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
+from .models import Post
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
-def home(request):
+#def home(request):
     # Example of normal function below:
     ###################################
     # object_user = ObjectModel.all()
     # context = {'all_users': object_user}
     # return render(request, 'blog_app/home.html', context)
-    return render(request, 'blog_app/home.html')
+    #return render(request, 'blog_app/home.html')
+
+class HomeView(ListView):
+    model = Post
+    template_name = 'blog_app/home.html'
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog_app/post.html'
 
 def login(request):
     # Example of normal function below:
