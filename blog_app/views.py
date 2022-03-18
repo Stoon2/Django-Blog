ba7a2c18bd060da9706e40119644e8792fafd52b
Stoon <melsayed.se@gmail.com>
2022-03-18 11:58:33 +0200

    added basic templating and styling, login, signup, bootstrap

--- blog_app/views.py
+++ blog_app/views.py
@@ -9,3 +9,19 @@ def home(request):
     # return render(request, 'blog_app/home.html', context)
     return render(request, 'blog_app/home.html')
 
+def login(request):
+    # Example of normal function below:
+    ###################################
+    # object_user = ObjectModel.all()
+    # context = {'all_users': object_user}
+    # return render(request, 'blog_app/home.html', context)
+    return render(request, 'blog_app/login.html')
+
+def signup(request):
+    # Example of normal function below:
+    ###################################
+    # object_user = ObjectModel.all()
+    # context = {'all_users': object_user}
+    # return render(request, 'blog_app/home.html', context)
+    return render(request, 'blog_app/signup.html')
+
