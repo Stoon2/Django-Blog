50bd30dd20ac2b48aae66a82d9aedba4a910e4a5
ahmedhassan1999 <ahmedelcoach30@gmail.com>
2022-03-19 21:03:32 +0200

    fix syntax error in urls file

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
     path('signup', views.signup, name='signup'),
-
     path('add_category/', views.add_cat, name="add_cat"),
+]
