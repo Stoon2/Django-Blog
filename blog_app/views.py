71dd5890073e4a13875e608d2f60a02e7df7b2b0
Menna97 <menna.elgedawy9761@gmail.com>
2022-03-23 21:53:38 +0200

    add remove like from post feature

--- blog_app/views.py
+++ blog_app/views.py
@@ -154,12 +154,23 @@ class PostDetailView(DetailView):
         context = super(PostDetailView, self).get_context_data()
         post = get_object_or_404(Post, id=self.kwargs['pk'])
         total_likes = post.total_likes()
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
         context["total_likes"] = total_likes
+        context["liked"] = liked
         return context
 
 def LikeView(request, pk):
     post = get_object_or_404(Post, id=request.POST.get('post_id'))
-    post.likes.add(request.user)
+    liked = False
+    if post.likes.filter(id=request.user.id).exists():
+        post.likes.remove(request.user)
+        liked = False
+    else:
+        post.likes.add(request.user)
+        liked = True
+
     return HttpResponseRedirect(reverse('post-detail', args=[str(pk)]))
 
 def loginPG(request):
