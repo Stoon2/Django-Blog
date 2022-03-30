df85708caf4aad31e5a5b6cf62185373891ddaa6
AmlAbdellatif <Amal.17295776@compit.edu.aun.eg>
2022-03-30 02:09:30 +0200

    Merge branch 'master' of github.com:Stoon2/Django-Blog

--- pydj_project/settings.py
+++ pydj_project/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog_app',
     'crispy_forms'
+    
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
